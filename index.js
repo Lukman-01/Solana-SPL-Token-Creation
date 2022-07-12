3c17a5a12e504ea981e5e6be0af1c2cdd3c5d200
gyan0890 <gyanlakshmi@gmail.com>
2022-07-12 17:23:52 +0530

    Committing the code

---
+++ index.js
@@ -0,0 +1,23 @@
+import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL } from '@solana/web3.js';
+import { createMint, getOrCreateAssociatedTokenAccount, mintTo, transfer } from '@solana/spl-token';
+
+(async () => {
+    // Step 1: Connect to cluster and generate a new Keypair
+    
+
+    // Step 2: Airdrop SOL into your from wallet
+    
+    // Step 3: Create new token mint and get the token account of the fromWallet address
+    //If the token account does not exist, create it
+    
+    
+    //Step 4: Mint a new token to the from account
+    
+
+    //Step 5: Get the token account of the to-wallet address and if it does not exist, create it
+    
+
+    //Step 6: Transfer the new token to the to-wallet's token account that was just created
+    // Transfer the new token to the "toTokenAccount" we just created
+ 
+})();
