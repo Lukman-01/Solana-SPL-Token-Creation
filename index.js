064d480767b0f949bb11f6a76eb8686fd8433835
Lukman Abdulyekeen <abdulyekeenlukman@gmail.com>
2023-11-14 20:58:30 +0000

    other steps implemented

--- index.js
+++ index.js
@@ -6,43 +6,66 @@ import { createMint, getOrCreateAssociatedTokenAccount, mintTo, transfer } from 
     
     const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
     // Get Keypair from Secret Key
-    var from = Keypair.generate();
+    var fromWallet = Keypair.generate();
 
     // Generate another Keypair (account we'll be sending to)
-    const to = Keypair.generate();
+    const toWallet = Keypair.generate();
 
     // Step 2: Airdrop 2 SOL into your from wallet
     console.log("Airdopping some SOL to Sender wallet!");
-    const fromAirDropSignature = await connection.requestAirdrop(
-        new PublicKey(from.publicKey),
-        2 * LAMPORTS_PER_SOL
-    );
-
-    // Latest blockhash (unique identifer of the block) of the cluster
-    let latestBlockHash = await connection.getLatestBlockhash();
+    
+    const fromAirDropSignature = await connection.requestAirdrop(fromWallet.publicKey, 2 * LAMPORTS_PER_SOL);
 
     // Confirm transaction using the last valid block height (refers to its time)
     // to check for transaction expiration
-    await connection.confirmTransaction({
-        blockhash: latestBlockHash.blockhash,
-        lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
-        signature: fromAirDropSignature
-    });
+    await connection.confirmTransaction(fromAirDropSignature, { commitment: "confirmed"});
 
     console.log("Airdrop completed for the Sender account");
     
     
     // Step 3: Create new token mint and get the token account of the fromWallet address
     //If the token account does not exist, create it
+    const mint = await createMint(connection,fromWallet,fromWallet.publicKey,null,9);
     
+    const fromTokenAcct = getOrCreateAssociatedTokenAccount(
+        connection,
+        fromWallet,
+        mint,
+        fromWallet.publicKey
+    );
     
     //Step 4: Mint a new token to the from account
-    
+    let signature = mintTo(
+        connection,
+        fromWallet,
+        mint,
+        (await fromTokenAcct).address,
+        fromWallet.publicKey,
+        1000000000,
+        []
+    );
+    console.log("Mint tx", signature);
 
     //Step 5: Get the token account of the to-wallet address and if it does not exist, create it
-    
+    const toTokenAcct = getOrCreateAssociatedTokenAccount(
+        connection,
+        fromWallet,
+        mint,
+        toWallet.publicKey
+    );
 
     //Step 6: Transfer the new token to the to-wallet's token account that was just created
     // Transfer the new token to the "toTokenAccount" we just created
+
+    signature = await transfer(
+        connection,
+        fromWallet,
+        (await fromTokenAcct).address,
+        (await toTokenAcct).address,
+        fromWallet.publicKey,
+        1000000000,
+        []
+    );
+    console.log("Transfer tx", signature);
  
 })();
